fed102fd89df922c38afce75d6c8cab47b1f427c
laurigurev <larigur@gmail.com>
2023-01-08 15:02:34 +0200

    redid the sorting

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 #include <vector>
+// #include <functional>
+#include <algorithm>
 
 struct edge {
         int src;
@@ -25,64 +27,47 @@ int main()
         };
 
         std::vector<int> nodes = {2, 3, 5, 7, 8, 9, 10, 11};
+
+        std::sort(edges.begin(), edges.end(), [](const edge e0, const edge e1) { return e0.dst < e1.dst; });
+
+        printf("Sorted edges\n");
+        for (edge e : edges) {
+                printf("{%i, %i}\n", e.src, e.dst);
+        }
+
+        std::vector<int> empty = {3, 5, 7};
         std::vector<int> sorted;
         sorted.reserve(9);
-        std::vector<int> levels;
-        levels.reserve(9);
-        std::vector<int> history;
-        history.reserve(9);
-        std::vector<int> marked;
-        marked.reserve(9);
 
-        while (!nodes.empty()) {
-                for (int i = 0; i < nodes.size(); i++) {
-                        marked.clear();
-                        int tmp = nodes[i];
-                        int counter = 0;
-                        
-                        printf("node %i, nodes.size() %llu, edges.size() %llu\n", tmp, nodes.size(), edges.size());
-                        for (int k = 0; k < edges.size(); k++) {
-                                printf("{%i, %i}, ", edges[k].src, edges[k].dst);
-                        }
-                        if (edges.size()) printf("\n");
-                        
-                        for (int ii = 0; ii < edges.size(); ii++) {
-                                if (edges[ii].dst == tmp) counter++;
-                                if (edges[ii].src == tmp) { 
-                                        marked.push_back(ii);
-                                        history.push_back(edges[ii].dst);
-                                }
-                        }
-                        
-                        if (counter) continue;
-                        sorted.push_back(tmp);
-                        nodes.erase(nodes.begin() + i);
+        for (int i = 0; i < empty.size();) {
+                int tmp = empty[i];
+                empty.erase(empty.begin());
+                sorted.push_back(tmp);
 
-                        for (int j = 0; j < history.size(); j++) {
-                                if (tmp == history[j]) {
-                                        levels.push_back(sorted.size() - 2);
-                                        history.clear();
-                                        break;
+                for (int j = 0; j < edges.size();) {
+                        if (edges[j].src == tmp) {
+                                int m = edges[j].dst;
+                                edges.erase(edges.begin() + j);
+                                for (edge e : edges) {
+                                        if (e.dst == m) {
+                                                m = -1;
+                                                break;
+                                        }
                                 }
+                                if (m != -1) empty.push_back(m);
                         }
-                        
-                        if (marked.size() == 0) break;
-                        for (int j = marked.size() - 1; j >= 0; j--) {
-                                edges.erase(edges.begin() + marked[j]);
-                        }
-                        
-                        break; // let's start at the beginning because vector dimensions have changed
+                        else j++;
                 }
         }
 
-        int li = 0;
-        for (int i = 0; i < sorted.size(); i++) {
-                printf("%i ", sorted[i]);
-                // printf("(li %i, i %i) ", levels[li], i);
-                if (levels[li] == i && levels.size()) {
-                        printf("\n");
-                        li++;
-                }
+        printf("\nSorted nodes\n");
+        for (int s : sorted) {
+                printf("%i ", s);
+        }
+
+        if (edges.size()) {
+                printf("Some edges left!\n");
+                return 1;
         }
 
         return 0;
